55b76ae7e6ac10fbba797d13a62e83ecec11b0af
Thomas Grove <tomgrove1996@gmail.com>
2022-07-29 07:45:48 -0700

    update

--- src/ComponentBuilder.ts
+++ src/ComponentBuilder.ts
@@ -12,8 +12,63 @@ export class ComponentBuilder {
     return this;
   }
 
+  setPosition(position: string): ComponentBuilder {
+    this.component.props.set("position", position);
+    return this;
+  }
+
+  setTop(top: string): ComponentBuilder {
+    this.component.props.set("top", top);
+    return this;
+  }
+
+  setBottom(bottom: string): ComponentBuilder {
+    this.component.props.set("bottom", bottom);
+    return this;
+  }
+
+  setLeft(left: string): ComponentBuilder {
+    this.component.props.set("left", left);
+    return this;
+  }
+
+  setRight(right: string): ComponentBuilder {
+    this.component.props.set("right", right);
+    return this;
+  }
+
+  setZIndex(zIndex: string): ComponentBuilder {
+    this.component.props.set("z-index", zIndex);
+    return this;
+  }
+
+  setDisplay(display: string): ComponentBuilder {
+    this.component.props.set("display", display);
+    return this;
+  }
+
+  setOverflow(overflow: string): ComponentBuilder {
+    this.component.props.set("overflow", overflow);
+    return this;
+  }
+
+  setOverflowStyle(overflowStyle: string): ComponentBuilder {
+    this.component.props.set("overflow-style", overflowStyle);
+    return this;
+  }
+
   setWidth(width: string): ComponentBuilder {
     this.component.props.set("width", width);
+    return this;
+  }
+
+  setMinWidth(minWidth: string): ComponentBuilder {
+    this.component.props.set("min-width", minWidth);
+    return this;
+  }
+
+  setMaxWidth(maxWidth: string): ComponentBuilder {
+    this.component.props.set("max-width", maxWidth);
     return this;
   }
 
@@ -22,13 +77,63 @@ export class ComponentBuilder {
     return this;
   }
 
+  setMaxHeight(maxHeight: string): ComponentBuilder {
+    this.component.props.set("max-height", maxHeight);
+    return this;
+  }
+
+  setMinHeight(minHeight: string): ComponentBuilder {
+    this.component.props.set("min-height", minHeight);
+    return this;
+  }
+
   setPadding(padding: string): ComponentBuilder {
     this.component.props.set("padding", padding);
     return this;
   }
 
+  setPaddingTop(paddingTop: string): ComponentBuilder {
+    this.component.props.set("padding-top", paddingTop);
+    return this;
+  }
+
+  setPaddingBottom(paddingBottom: string): ComponentBuilder {
+    this.component.props.set("padding-bottom", paddingBottom);
+    return this;
+  }
+
+  setPaddingLeft(paddingLeft: string): ComponentBuilder {
+    this.component.props.set("padding-left", paddingLeft);
+    return this;
+  }
+
+  setPaddingRight(paddingRight: string): ComponentBuilder {
+    this.component.props.set("padding-right", paddingRight);
+    return this;
+  }
+
   setMargin(margin: string): ComponentBuilder {
     this.component.props.set("margin", margin);
+    return this;
+  }
+
+  setMarginTop(marginTop: string): ComponentBuilder {
+    this.component.props.set("margin-top", marginTop);
+    return this;
+  }
+
+  setMarginBottom(marginBottom: string): ComponentBuilder {
+    this.component.props.set("margin-bottom", marginBottom);
+    return this;
+  }
+
+  setMarginLeft(marginLeft: string): ComponentBuilder {
+    this.component.props.set("margin-left", marginLeft);
+    return this;
+  }
+
+  setMarginRight(marginRight: string): ComponentBuilder {
+    this.component.props.set("margin-right", marginRight);
     return this;
   }
 
@@ -52,8 +157,33 @@ export class ComponentBuilder {
     return this;
   }
 
+  setFontStyle(fontStyle: string): ComponentBuilder {
+    this.component.props.set("font-style", fontStyle);
+    return this;
+  }
+
+  setTextDirection(textDirection: string): ComponentBuilder {
+    this.component.props.set("direction", textDirection);
+    return this;
+  }
+
   setTextAlign(align: string): ComponentBuilder {
     this.component.props.set("text-align", align);
+    return this;
+  }
+
+  setTextOutline(textOutline: string): ComponentBuilder {
+    this.component.props.set("text-outline", textOutline);
+    return this;
+  }
+
+  setTextDecoration(textDecoration: string): ComponentBuilder {
+    this.component.props.set("text-decoration", textDecoration);
+    return this;
+  }
+
+  setTextShadow(textShadow: string): ComponentBuilder {
+    this.component.props.set("text-shadow", textShadow);
     return this;
   }
 
@@ -62,32 +192,90 @@ export class ComponentBuilder {
     return this;
   }
 
-  build() {
-    this.buildComponent(this.component);
+  setBorder(border: string): ComponentBuilder {
+    this.component.props.set("border", border);
+    return this;
   }
 
-  buildComponent(root: Component) {
+  setBorderLeft(borderLeft: string): ComponentBuilder {
+    this.component.props.set("border-left", borderLeft);
+    return this;
+  }
+
+  setBorderRight(borderRight: string): ComponentBuilder {
+    this.component.props.set("border-right", borderRight);
+    return this;
+  }
+
+  setBorderTop(borderTop: string): ComponentBuilder {
+    this.component.props.set("border-top", borderTop);
+    return this;
+  }
+
+  setBorderBottom(borderBottom: string): ComponentBuilder {
+    this.component.props.set("border-bottom", borderBottom);
+    return this;
+  }
+
+  setBorderRadius(borderRadius: string): ComponentBuilder {
+    this.component.props.set("border-radius", borderRadius);
+    return this;
+  }
+
+  setBorderRadiusTopLeft(borderRadiusTopLeft: string): ComponentBuilder {
+    this.component.props.set("border-top-left-radius", borderRadiusTopLeft);
+    return this;
+  }
+
+  setBorderRadiusTopRight(borderRadiusTopRight: string): ComponentBuilder {
+    this.component.props.set("border-top-right-radius", borderRadiusTopRight);
+    return this;
+  }
+
+  setBorderRadiusBottomRight(
+    borderRadiusBottomRight: string
+  ): ComponentBuilder {
+    this.component.props.set(
+      "border-bottom-right-radius",
+      borderRadiusBottomRight
+    );
+    return this;
+  }
+
+  setBorderRadiusBottomLeft(borderRadiusBottomLeft: string): ComponentBuilder {
+    this.component.props.set(
+      "border-bottom-left-radius",
+      borderRadiusBottomLeft
+    );
+    return this;
+  }
+
+  private buildComponent(root: Component) {
     if (!root) return;
 
-    let q: Component[] = [];
+    const q: Component[] = [];
     q.push(root);
 
     while (q.length > 0) {
-      let component = q.shift();
+      const component = q.shift();
       if (!component) return;
 
       if (component === root)
         document.body.insertAdjacentHTML("beforeend", root.getHTML());
 
       const children = component.children.length;
-
       for (let i = 0; i < children; i++) {
         const parent = document.getElementById(component.id);
         const child = component.children[i];
+
         if (parent) parent.insertAdjacentHTML("beforeend", child.getHTML());
         q.push(child);
       }
     }
+  }
+
+  build() {
+    this.buildComponent(this.component);
   }
 
   add(component: Component) {
--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import { ComponentBuilder } from "./ComponentBuilder";
 
 const bg = new ComponentBuilder("bg")
   .setPadding("1rem")
-  .setWidth("50vw")
-  .setHeight("50vh")
+  .setWidth("100vw")
+  .setHeight("100vh")
+  .setBorder("3px solid red")
+  .setBorderRadius("10px")
   .setBackgroundColor("rgb(0, 0, 0)");
 
 const title = new ComponentBuilder("title")
@@ -12,5 +14,17 @@ const title = new ComponentBuilder("title")
   .setFontColor("white")
   .setTextAlign("center");
 
+const box = new ComponentBuilder("box")
+  .setPosition("fixed")
+  .setTop("1rem")
+  .setLeft("1rem")
+  .setWidth("100px")
+  .setHeight("100px")
+  .setBorderRadiusTopLeft("10px")
+  .setBorderRadiusBottomRight("10px")
+  .setBackgroundColor("red");
+
 bg.add(title.getComponent());
+bg.add(box.getComponent());
+
 bg.build();
