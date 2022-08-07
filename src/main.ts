7267c8c8b812129db13806e11a67a5f2bc4fe5f8
Thomas Grove <tomgrove1996@gmail.com>
2022-08-07 12:13:59 -0700

    UI

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import "./style.css";
-import { BasicContainer } from "./components/BasicContainer";
-import { TextBox } from "./components/TextBox";
+import { BasicContainer, TextBox } from "./UI";
 
-const container = new BasicContainer("container", "50vw", "50vh");
-const text = new TextBox("sometext", "Text");
+const container = BasicContainer("container", "50vw", "50vh");
+const text = TextBox("sometext", "Text");
 
 container.add(text);
 
-container.pack();
+container.build();
 
 console.log(document.body.innerHTML);
