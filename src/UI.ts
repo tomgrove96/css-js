4b69137f55b28d48db3a323514cdd2954ec5f0d9
Nathan Grove <nathangrove08@gmail.com>
2022-08-16 20:33:09 -0700

    nothing

--- src/UI.ts
+++ src/UI.ts
@@ -1,41 +1,44 @@
-import { ComponentBuilder } from "./builder/ComponentBuilder";
+import { Component } from "./builder/Component";
 import * as Type from "./Type";
 import * as Color from "./Color";
 
 export const BasicContainer = (id: string, width: Type.unit, height: Type.unit) => {
-  return new ComponentBuilder(id)
+  return new Component(id)
     .setWidth(width)
     .setHeight(height)
-    .setBorder("solid", "10px", new Color.RGBA(0, 0, 0, 0.5))
     .setBackgroundColor(Color.RGBA.fromRGB(Color.SLATE_GRAY, 0.5))
-    .setBorderRadius("16px")
-    .setFont("Arial")
-    .setFontSize("16px")
-    .setTextAlign("center")
-    .setFontColor(Color.WHITE)
-    .setPlacement("relative");
+    .setPlacement("relative")
+    .setBorder("solid", "thin", Color.BLACK);
 };
 
-export const TextBox = (id: string, value: string) => {
-  return new ComponentBuilder(id)
-    .setValue(value)
-    .setFont("Arial")
-    .setFontSize("16px")
-    .setTextAlign("center")
-    .setFontColor(Color.WHITE)
-    .setPlacement("absolute");
-};
+export const Text = (id: string, value: string) => {
+    return new Component(id, value)
+      .setBackgroundColor(Color.RGBA.fromRGB(Color.SLATE_GRAY, 0.5))
+      .setPlacement("relative")
+      .text!.setValue(value)
+      .text!.setTextAlign("center");
+  };
 
-export const Button = (id: string, label: string, width: Type.unit, height: Type.unit) => {
-  return new ComponentBuilder(id)
-    .setFont("Arial")
-    .setValue(label)
-    .setWidth(width)
-    .setHeight(height)
-    .setBorder("solid", "5px", new Color.RGBA(0, 0, 0, 0.5))
-    .setFont("Arial")
-    .setFontSize("16px")
-    .setTextAlign("center")
-    .setFontColor(Color.WHITE)
-    .setPlacement("absolute");
-};
+// export const TextBox = (id: string, value: string) => {
+//   return new ComponentBuilder(id)
+//     .setValue(value)
+//     .setFont("Arial")
+//     .setFontSize("16px")
+//     .setTextAlign("center")
+//     .setFontColor(Color.WHITE)
+//     .setPlacement("absolute");
+// };
+
+// export const Button = (id: string, label: string, width: Type.unit, height: Type.unit) => {
+//   return new ComponentBuilder(id)
+//     .setFont("Arial")
+//     .setValue(label)
+//     .setWidth(width)
+//     .setHeight(height)
+//     .setBorder("solid", "5px", new Color.RGBA(0, 0, 0, 0.5))
+//     .setFont("Arial")
+//     .setFontSize("16px")
+//     .setTextAlign("center")
+//     .setFontColor(Color.WHITE)
+//     .setPlacement("absolute");
+// };
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,17 @@
 import "./style.css";
-import * as Color from "./Color";
-import { BasicContainer, TextBox, Button } from "./UI";
+import { BasicContainer, Text } from "./UI";
+import * as Color from "./Color"
 
 const container = BasicContainer("container", "450px", "600px").setPosition("TOP_CENTER");
-const text = TextBox("text", "Text").setPosition("TOP_CENTER");
-const button = Button("button", "button", "96px", "48px").setPosition("TOP_RIGHT");
+const text = Text("text", "Text").setPosition("TOP_CENTER");
+// const button = Button("button", "button", "96px", "48px").setPosition("TOP_RIGHT");
+const container2 = BasicContainer("container", "450px", "600px").setPosition("TOP_CENTER");
 
 text.setBackgroundColor(Color.ALICE_BLUE);
 container.add(text);
-container.add(button);
+//container.add(button);
 
 container.build();
+container2.build();
 
 console.log(document.body.innerHTML);
